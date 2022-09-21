3ee29ef47c028f8944cd66a4ad216267c878f3bd
Yago Matias <109563727+YagoMatias@users.noreply.github.com>
2022-09-21 09:59:23 -0300

    att0.0.2

---
+++ cursos/origamid/javascript/dom-para-iniciantes/classes-e-atributos/script.js
@@ -0,0 +1,19 @@
+// Adicione a classe ativo a todos os itens do menu
+
+const menu = document.querySelectorAll('.menu');
+console.log(menu);
+
+
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+
+// menu.classList.remove('ativo');
+
+// Verifique se as imagens possuem o atributo alt
+
+// Modifique o href do link externo no menu
+
+
+
+
+
