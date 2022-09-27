a053ad12a1a4d8c631fc27fff7659a56b55c94b2
YagoMatias <114001591+YagoMatiasDev@users.noreply.github.com>
2022-09-27 12:40:39 -0300

    update teste

--- projetos/teste/script.js
+++ projetos/teste/script.js
@@ -1,14 +1,19 @@
-const filmes = document.querySelectorAll(".ficcao  p");
-const sinopse = document.querySelectorAll(".sinopse p");
-
-function activeTab(filme) {
-  filme.classList.add("ativo");
+function initTabNav() {
+  const filmes = document.querySelectorAll(".ficcao a");
+  const sinopses = document.querySelectorAll(".fsinopse p");
+  if (sinopses.length && filmes.length) {
+    sinopses[0].classList.add("ativo");
+    function activeTab(index) {
+      sinopses.forEach((sinopse) => {
+        sinopse.classList.remove("ativo");
+      });
+      sinopses[index].classList.add("ativo");
+    }
+    filmes.forEach((filme, index) => {
+      filme.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
 }
-
-filmes.forEach((item) => {
-  item.addEventListener("click", activeTab);
-});
-
-sinopse.forEach((item) => {
-  console.log(item);
-});
+initTabNav();
