ccca328255dd4513d93854bee5ab099f134c5256
Yago Matias <109563727+YagoMatias@users.noreply.github.com>
2022-09-29 23:16:19 -0300

    update gtasa

--- projetos/gta-sa/gta/script.js
+++ projetos/gta-sa/gta/script.js
@@ -1,4 +1,23 @@
-const menu = document.querySelectorAll('.js-menu a[href^="#"');
+function scrollNav() {
+  const menu = document.querySelectorAll('.js-menu a[href^="#"');
+
+  function scrollLink(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }
+
+  menu.forEach((link) => {
+    link.addEventListener("click", scrollLink);
+  });
+}
+scrollNav();
+
+const arrow = document.querySelectorAll('a[href^="#"');
 
 function scrollLink(event) {
   event.preventDefault();
@@ -10,6 +29,6 @@ function scrollLink(event) {
   });
 }
 
-menu.forEach((link) => {
+arrow.forEach((link) => {
   link.addEventListener("click", scrollLink);
 });
