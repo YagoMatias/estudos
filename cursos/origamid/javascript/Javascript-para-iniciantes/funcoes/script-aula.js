d6910fc5327b3363e6f27972306fdcdd97794234
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-10-31 22:10:28 -0300

    gta-sa-2.0 att

---
+++ cursos/origamid/javascript/Javascript-para-iniciantes/funcoes/script-aula.js
@@ -0,0 +1,10 @@
+function quadrado(lado) {
+  return lado * lado;
+}
+quadrado(4);
+
+function click() {
+  console.log("clicou");
+}
+
+addEventListener("click", click);
--- gta-sa-2.0/script.js
+++ gta-sa-2.0/script.js
@@ -1,17 +1,35 @@
-const links = document.querySelectorAll('a[href^="#"');
+function scrollSuave() {
+  const links = document.querySelectorAll('a[href^="#"');
 
-function scroll(event) {
-  console.log('click');
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelector(href);
-  section.scrollIntoView ({
-    behavior: "smooth",
+  function scroll(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
       block: "center",
+    });
+  }
+
+  links.forEach((link) => {
+    link.addEventListener("click", scroll);
   });
-};
+}
+scrollSuave();
 
+function sectionScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const metadeWindow = window.innerHeight * 0.6;
 
-links.forEach(link => {
-  link.addEventListener('click', scroll);
-});
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const sectionVisible = sectionTop - metadeWindow < 0;
+      if (sectionVisible) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+  window.addEventListener("scroll", animaScroll);
+}
+sectionScroll();
