7c0755680e56afd96fb705cee68c6517ab8f2d57
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-03 12:06:25 -0300

    gta-sa-2.0-att

--- gta-sa-2.0/script.js
+++ gta-sa-2.0/script.js
@@ -1,3 +1,4 @@
+function scrollNav(){
 const links = document.querySelectorAll('a[href^="#');
 console.log(links);
 
@@ -14,21 +15,23 @@ function scrollSuave(event){
 links.forEach((link)=> {
   link.addEventListener("click", scrollSuave);
 })
+}
+scrollNav();
 
-// function sectionScroll() {
-//   const sections = document.querySelectorAll(".js-scroll");
-//   const metadeWindow = window.innerHeight * 0.75;
+function sectionScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  const metadeWindow = window.innerHeight * 0.75;
 
-//   function animaScroll() {
-//     sections.forEach((section) => {
-//       const sectionTop = section.getBoundingClientRect().top;
-//       const sectionVisible = sectionTop - metadeWindow < 0;
-//       if (sectionVisible) {
-//         section.classList.add("ativo");
-//       }
-//     });
-//   }
-//   animaScroll();
-//   window.addEventListener("scroll", animaScroll);
-// }
-// sectionScroll();
+  function animaScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const sectionVisible = sectionTop - metadeWindow < 0;
+      if (sectionVisible) {
+        section.classList.add("ativo");
+      }
+    });
+  }
+  animaScroll();
+  window.addEventListener("scroll", animaScroll);
+}
+sectionScroll();
