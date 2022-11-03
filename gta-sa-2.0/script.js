9f7101901f502e2f3e6feaef77decde8c4f4f0b7
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-03 07:36:59 -0300

    gta-sa-2.0-att

--- gta-sa-2.0/script.js
+++ gta-sa-2.0/script.js
@@ -1,36 +1,34 @@
-function scrollSuave() {
-  const links = document.querySelectorAll('a[href^="#"');
+const links = document.querySelectorAll('a[href^="#');
+console.log(links);
 
-  function scroll(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }
-
-  links.forEach((link) => {
-    link.addEventListener("click", scroll);
-  });
+function scrollSuave(event){
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
+  const section = document.querySelector(href);
+  section.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  })
 }
-scrollSuave();
 
-function sectionScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
-  const metadeWindow = window.innerHeight * 0.75;
+links.forEach((link)=> {
+  link.addEventListener("click", scrollSuave);
+})
 
-  function animaScroll() {
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const sectionVisible = sectionTop - metadeWindow < 0;
-      if (sectionVisible) {
-        section.classList.add("ativo");
-      }
-    });
-  }
-  animaScroll();
-  window.addEventListener("scroll", animaScroll);
-}
-sectionScroll();
+// function sectionScroll() {
+//   const sections = document.querySelectorAll(".js-scroll");
+//   const metadeWindow = window.innerHeight * 0.75;
+
+//   function animaScroll() {
+//     sections.forEach((section) => {
+//       const sectionTop = section.getBoundingClientRect().top;
+//       const sectionVisible = sectionTop - metadeWindow < 0;
+//       if (sectionVisible) {
+//         section.classList.add("ativo");
+//       }
+//     });
+//   }
+//   animaScroll();
+//   window.addEventListener("scroll", animaScroll);
+// }
+// sectionScroll();
