d770709173fe64397899ccc892230b46cbf67b98
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-04 12:22:13 -0300

    gta-sa-2.0 att

--- gta-sa-2.0/script.js
+++ gta-sa-2.0/script.js
@@ -35,3 +35,17 @@ function sectionScroll() {
   window.addEventListener("scroll", animaScroll);
 }
 sectionScroll();
+
+function Imagens(seletor) {
+  this.element = function(){
+    return document.querySelectorAll(seletor);
+  }
+  this.ativar = function(classe){
+    this.element().forEach((item)=>{
+      item.classList.add(classe);
+    });
+  }
+}
+
+const imgs = new Imagens('section div > img')
+imgs.ativar('imagem')
---
+++ projetos/teste/script.js
@@ -0,0 +1,13 @@
+// function Dom(seletor) {
+//   this.element = function(){
+//     return document.querySelectorAll(seletor)
+//   };
+//   this.ativar = function(classe){
+//     this.element().forEach((item) =>{
+//       item.classList.add(classe)
+//     })
+//   }
+// }
+// const caixa = new Dom("div.bloco");
+// console.log(caixa.element(caixa));
+// caixa.ativar('ativo')
