46691da50513d0bc266d00a08559d60d29f6e4b1
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-08 22:39:40 -0300

    origamid

---
+++ cursos/origamid/javascript/objetos/string/script.js
@@ -0,0 +1,51 @@
+// const esportes = "Esportes";
+// console.log(esportes.length);
+// const basquete = new String("basquete");
+
+// console.log(basquete[0]);
+// console.log(basquete[basquete.length - 1]);
+// console.log(basquete.charAt(basquete.length - 1));
+
+// const frase = "macacos me ";
+// const mordam = "mordendo";
+// const socorro = ", socorro !!!";
+
+// console.log(frase.concat(mordam, socorro));
+
+// const esportes = "Futebol";
+// const listaEsportes = "Basquete, Futebol, Natação";
+
+// console.log(listaEsportes.includes(esportes));
+// console.log(esportes.includes(listaEsportes));
+
+// console.log(esportes.startsWith("Fu"));
+// console.log(esportes.endsWith("Fu"));
+
+// const esporte1 = "basquete";
+// const esporte2 = "futebol";
+// const esporte3 = "natação";
+
+// console.log(esporte1.slice(0, -5));
+
+// console.log(esportes.indexOf("e"));
+
+// const preco = "R$ 100.00";
+// console.log(preco.padEnd(26, " Caro pra caralho "));
+
+// const listaPreco = ["R$ 100", "R$ 1000", "R$ 2000"];
+
+// listaPreco.forEach((item) => {
+//   console.log(item.padStart(10, "-"));
+// });
+
+// let listaItem = "camisa, boné, calça, bota.";
+
+// listaItem = listaItem.replace(/[,]+/g, ".");
+
+let lista = document.querySelectorAll("li");
+
+lista.forEach((item) => {
+  let velho = item.innerHTML.split("li");
+  let novo = velho.join("div");
+  console.log(novo);
+});
