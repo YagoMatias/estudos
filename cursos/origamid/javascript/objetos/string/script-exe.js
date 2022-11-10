79450497e901032157965a1f57538685424c4fe2
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-09 23:14:23 -0300

    origamid

--- cursos/origamid/javascript/objetos/string/script-exe.js
+++ cursos/origamid/javascript/objetos/string/script-exe.js
@@ -3,38 +3,71 @@
 
 const transacoes = [
   {
-    descricao: 'Taxa do Pão',
-    valor: 'R$ 39',
+    descricao: "Taxa do Pão",
+    valor: "R$ 39",
   },
   {
-    descricao: 'Taxa do Mercado',
-    valor: 'R$ 129',
+    descricao: "Taxa do Mercado",
+    valor: "R$ 129",
   },
   {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 99',
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 99",
   },
   {
-    descricao: 'Taxa do Banco',
-    valor: 'R$ 129',
+    descricao: "Taxa do Banco",
+    valor: "R$ 129",
   },
   {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 49',
+    descricao: "Recebimento de Cliente",
+    valor: "R$ 49",
   },
 ];
 
-let total = 0
+let taxas = 0;
 
-transacoes.forEach((item) =>{
+transacoes.forEach((item) => {
   const numero = +item.valor.slice(3);
- if(item.descricao.includes("Taxa"))
- total = total + numero
+  if (item.descricao.includes("Taxa")) taxas = taxas + numero;
 });
 
+let recebimento = 0;
+transacoes.forEach((item) => {
+  const numero = +item.valor.slice(3);
+  if (item.descricao.includes("Rece")) recebimento = recebimento + numero;
+});
 
+console.log(recebimento);
 
+// Retorne uma array com a lista abaixo
+const transportes = "Carro;Avião;Trem;Ônibus;Bicicleta";
 
+const arrayTransportes = transportes.split(";");
 
+// Substitua todos os span's por a's
 
+const html =
+  "<ul><li><span>Sobre</span></li><li><span>Produtos</span></li><li><span>Contato</span></li></ul>";
 
+const htmlArray = html.split("span");
+const htmlLink = htmlArray.join("a");
+
+console.log(htmlLink);
+
+// Retorne o último caracter da frase
+const frase = "Melhor do ano!";
+
+console.log(frase.slice(-1));
+
+// Retorne o total de taxas
+const transacoes1 = [
+  "Taxa do Banco",
+  "   TAXA DO PÃO",
+  "  taxa do mercado",
+  "depósito Bancário",
+  "TARIFA especial",
+];
+
+transacoes1.forEach((item) => {
+  console.log(item.toUpperCase().includes("TAXA"));
+});
