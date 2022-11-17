b778c691d105988188a79ae835c25ead9353f0b4
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-16 22:52:30 -0300

    origamid

---
+++ cursos/origamid/javascript/objetos/funcoes/array-e-iteracao/script-exe.js
@@ -0,0 +1,16 @@
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+
+// const caracteres = [...document.querySelectorAll("p")];
+
+// const quantidade = caracteres.reduce((anterior, item) => {
+//   const caracter = item.innerText.length;
+//   console.log(caracter);
+//   return caracter + anterior;
+// }, 0);
+
+// console.log(quantidade);
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
---
+++ cursos/origamid/javascript/objetos/funcoes/array-e-iteracao/script.js
@@ -0,0 +1,14 @@
+// function Dom(seletor) {
+//   this.element = document.querySelector(seletor);
+// }
+
+// Dom.prototype.ativo = function (classe) {
+//   this.element.classList.add(classe);
+//   console.log(this);
+// };
+
+// const li = {
+//   element: document.querySelector("li"),
+// };
+
+// Dom.prototype.ativo.call(li, "ativo");
