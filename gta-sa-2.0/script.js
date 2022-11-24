88b5d2815774f0a2a12fb3a431fa7e948ed44300
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-23 23:45:42 -0300

    gta-sa-2.0

--- gta-sa-2.0/script.js
+++ gta-sa-2.0/script.js
@@ -1,20 +1,20 @@
-function scrollNav(){
-const links = document.querySelectorAll('a[href^="#');
-console.log(links);
+function scrollNav() {
+  const links = document.querySelectorAll('a[href^="#');
+  console.log(links);
 
-function scrollSuave(event){
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute("href");
-  const section = document.querySelector(href);
-  section.scrollIntoView({
-    behavior: "smooth",
-    block: "center",
-  })
-}
+  function scrollSuave(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  }
 
-links.forEach((link)=> {
-  link.addEventListener("click", scrollSuave);
-})
+  links.forEach((link) => {
+    link.addEventListener("click", scrollSuave);
+  });
 }
 scrollNav();
 
@@ -25,7 +25,7 @@ function sectionScroll() {
   function animaScroll() {
     sections.forEach((section) => {
       const sectionTop = section.getBoundingClientRect().top;
-      const sectionVisible = (sectionTop - metadeWindow) < 0;
+      const sectionVisible = sectionTop - metadeWindow < 0;
       if (sectionVisible) {
         section.classList.add("ativo");
       }
@@ -37,15 +37,54 @@ function sectionScroll() {
 sectionScroll();
 
 function Imagens(seletor) {
-  this.element = function(){
+  this.element = function () {
     return document.querySelectorAll(seletor);
-  }
-  this.ativar = function(classe){
-    this.element().forEach((item)=>{
+  };
+  this.ativar = function (classe) {
+    this.element().forEach((item) => {
       item.classList.add(classe);
     });
-  }
+  };
 }
 
-const imgs = new Imagens('section div > img')
-imgs.ativar('imagem')
+const imgs = new Imagens("section div > img");
+imgs.ativar("imagem");
+
+const modal = document.querySelector(".modal-conteiner");
+const botaoAbrir = document.querySelector(".modal-link");
+const botaoFechar = document.querySelector(".modal-bnt");
+
+function abrirModal(event) {
+  event.preventDefault();
+  modal.classList.toggle("ativo");
+}
+
+// function fecharModal(event) {
+//   event.preventDefault();
+//   modal.classList.remove("ativo");
+// }
+
+function maquina() {
+  function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split("");
+    elemento.innerHTML = "";
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => (elemento.innerHTML += letra), 200 * i);
+    });
+  }
+
+  // Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
+  // function typeWriter(elemento) {
+  //   const textoArray = elemento.innerHTML.split('');
+  //   elemento.innerHTML = '';
+  //   for(let i = 0; i < textoArray.length; i++) {
+  //     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+  //   }
+  // }
+
+  const titulo = document.querySelector(".p-modal");
+  typeWriter(titulo);
+}
+botaoAbrir.addEventListener("click", abrirModal);
+botaoAbrir.addEventListener("click", maquina);
+botaoFechar.addEventListener("click", abrirModal);
