4a125522afe73acf169c674b076558eb1e526175
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-29 23:30:59 -0300

    origamid

--- cursos/origamid/javascript/efeitos-no-dom/modules-intro/script.js
+++ cursos/origamid/javascript/efeitos-no-dom/modules-intro/script.js
@@ -2,8 +2,10 @@ import initAccordion from "./modules/accordion.js";
 import initAnimacaoScroll from "./modules/anima-scroll.js";
 import ativadorMenu from "./modules/ativador-menu.js";
 import initScrollSuave from "./modules/scroll-suave.js";
+import initTooltip from "./modules/tooltip.js";
 
 initAccordion();
 initAnimacaoScroll();
 ativadorMenu();
 initScrollSuave();
+initTooltip();
