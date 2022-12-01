df280fc748ee3ad078869c343f1c4a44bde56bb5
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-11-30 22:33:34 -0300

    gta-sa-2.0 mobile

--- gta-sa-2.0/script.js
+++ gta-sa-2.0/script.js
@@ -3,9 +3,11 @@ import zoomImg from "./modules/zoom-img.js";
 import scrollNav from "./modules/scroll-nav.js";
 import modal from "./modules/modal.js";
 import typewriter from "./modules/typewriter.js";
+import menuHamburger from "./modules/menu-hamburger.js";
 
 animaScroll();
 zoomImg();
 scrollNav();
-// modal();
-// typewriter();
+modal();
+typewriter();
+menuHamburger();
