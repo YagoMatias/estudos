a47bb3d96fe82c89f01e79a83a6dd731e9645c1d
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-12-05 22:59:30 -0300

    gta-sa-2.0 update

--- gta-sa-2.0/modules/menu-hamburger.js
+++ gta-sa-2.0/modules/menu-hamburger.js
@@ -1,11 +1,10 @@
 export default function menuHamburger() {}
 
 const botao = document.querySelector(".botao button");
-console.log(botao);
 
 function hamburger(event) {
   event.preventDefault();
-  botao.classList.toggle('is-active')
+  botao.classList.toggle("is-active");
 }
 
 botao.addEventListener("click", hamburger);
--- gta-sa-2.0/script.js
+++ gta-sa-2.0/script.js
@@ -4,6 +4,7 @@ import scrollNav from "./modules/scroll-nav.js";
 import modal from "./modules/modal.js";
 import typewriter from "./modules/typewriter.js";
 import menuHamburger from "./modules/menu-hamburger.js";
+import menuMobile from "./modules/menu-mobile.js";
 
 animaScroll();
 zoomImg();
@@ -11,3 +12,4 @@ scrollNav();
 modal();
 typewriter();
 menuHamburger();
+menuMobile();
