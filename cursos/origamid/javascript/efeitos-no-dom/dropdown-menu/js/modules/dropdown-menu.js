2713454c0bb5b743e8f15d721e38ba4d0bde5fe1
YagoMatias <109563727+YagoMatias@users.noreply.github.com>
2022-12-11 15:08:47 -0300

    origamid update

--- cursos/origamid/javascript/efeitos-no-dom/dropdown-menu/js/modules/dropdown-menu.js
+++ cursos/origamid/javascript/efeitos-no-dom/dropdown-menu/js/modules/dropdown-menu.js
@@ -1,13 +1,24 @@
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
-  dropdownMenus.forEach(menu => {
-    ['touchstart', 'click'].forEach(userEvent => {
-      menu.addEventListener(userEvent, handleClick);
-    });
+export default function initDropdownMenu() {}
+const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+dropdownMenus.forEach((menu) => {
+  ["touchstart", "click"].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
   });
+});
 
-  function handleClick(event) {
-    event.preventDefault();
-    this.classList.add('active');
-  };
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.toggle("active");
+  outsideClick(this, () => {
+    console.log("ativou");
+  });
+}
+
+function outsideClick(element, callBack) {
+  const html = document.documentElement;
+  html.addEventListener("click", handleOutsideClick);
+  function handleOutsideClick(event) {
+    console.log(element);
+    callBack();
+  }
 }
